3155cd363a4713aaef418f1bef3acbb0a80ed4dd
Isabelleqqq <757489443@qq.com>
2018-11-21 20:24:54 +0800

    公共的功能完成（侧边栏切换，二级菜单切换，退出功能）

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,10 +1,45 @@
 $(function() {
+  // 1.进度条功能
+  // 第一个ajax请求开始时开始进度条
   $(document).ajaxStart(function() {
     NProgress.start();
   });
+  // 所有的ajax请求结束后关闭进度条
   $(document).ajaxStop(function() {
     setTimeout(function() {
       NProgress.done();
     }, 3000);
+  });
+
+  // 公共部分功能
+  // 1.左侧二级菜单切换功能
+  $("#category").click(function() {
+    // 找下一个兄弟元素
+    $(this)
+      .next()
+      .stop()
+      .slideToggle();
+  });
+  // 2.左侧菜单切换功能
+  $(".lt_topbar .icon_left").click(function() {
+    $(".lt_aside").toggleClass("hidemenu");
+    $(".lt_topbar").toggleClass("hidemenu");
+    $(".lt_main").toggleClass("hidemenu");
+  });
+  // 3.右侧公共退出功能
+  $(".lt_topbar .icon_right").click(function() {
+    $("#logoutModal").modal("show");
+  });
+  $("#logoutBtn").click(function() {
+    $.ajax({
+      type: "get",
+      url: "/employee/employeeLogout",
+      dataType: "json",
+      success: function(info) {
+        if (info.success) {
+          location.href = "login.html";
+        }
+      }
+    });
   });
 });
---
+++ public/back/js/isLogin.js
@@ -0,0 +1,16 @@
+// 需求：登录拦截，对未登录的用户，拦截到登录页
+// 由于前端不知道当前用户的登录状态，但后台知道，所以需要发送ajax请求，让后台判断
+$.ajax({
+  type: "get",
+  url: "/employee/checkRootLogin",
+  dataType: "json",
+  success: function(info) {
+    console.log(info);
+    if (info.success) {
+      console.log("该用户已登录");
+    }
+    if (info.error === 400) {
+      location.href = "login.html";
+    }
+  }
+});
